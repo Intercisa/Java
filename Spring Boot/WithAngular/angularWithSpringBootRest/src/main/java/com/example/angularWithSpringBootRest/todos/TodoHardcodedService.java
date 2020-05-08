697285c3f51466f118a6aeba341a11deaa210f6a
Intercisa <sipiczkibarna@gmail.com>
2020-05-08 16:50:47 +0200

    some updates

--- Spring Boot/WithAngular/angularWithSpringBootRest/src/main/java/com/example/angularWithSpringBootRest/todos/TodoHardcodedService.java
+++ Spring Boot/WithAngular/angularWithSpringBootRest/src/main/java/com/example/angularWithSpringBootRest/todos/TodoHardcodedService.java
@@ -26,6 +26,19 @@ public class TodoHardcodedService {
 		return todos;
 	}
 	
+	
+	public Todo save(Todo todo) {
+		if(todo.getId() == -1 || todo.getId() == 0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		}else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		
+		return todo;
+	}
+	
 	public Todo findById(long id) {
 		
 		for (Todo todo : todos) {
--- Spring Boot/WithAngular/angularWithSpringBootRest/src/main/java/com/example/angularWithSpringBootRest/todos/TodoResource.java
+++ Spring Boot/WithAngular/angularWithSpringBootRest/src/main/java/com/example/angularWithSpringBootRest/todos/TodoResource.java
@@ -1,14 +1,20 @@
 package com.example.angularWithSpringBootRest.todos;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -29,7 +35,6 @@ public class TodoResource {
 	}
 	
 	
-	
 	@DeleteMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(
 			@PathVariable String username, @PathVariable long id) {
@@ -40,6 +45,39 @@ public class TodoResource {
 			return ResponseEntity.noContent().build(); //send back a no content status - IMPORTANT 
 		
 		return ResponseEntity.notFound().build(); ////send back a not found status - IMPORTANT 
+	}
+	
+	//Edit/Update a Todo
+	//PUT 
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username, @PathVariable long id, @RequestBody Todo todo){
+		
+		Todo updatedTodo = todoService.save(todo);
+		
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);	
+	}
+	
+	
+	//POST
+	@PostMapping("/users/{username}/todos") //never leave a '/' on the end of the mapping uri 
+	public ResponseEntity<Void> saveTodo(
+			@PathVariable String username, @RequestBody Todo todo){
+
+		Todo createdTodo = todoService.save(todo);
+		
+		//LOCATION
+		//GET current resource url
+		///users/{username}/todos/{id}
+		
+		URI uri = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(createdTodo.getId())
+		.toUri();
+		
+		return ResponseEntity.created(uri).build();
+			
 	}
 	
 }
