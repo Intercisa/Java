b3145a03016872f2506d88130ccd282252549cc7
Intercisa <sipiczkibarna@gmail.com>
2020-06-02 09:54:08 +0200

    deleting node linked list

---
+++ LeetCode/junechallange/deletenodeInalinkedlist/test/deletenode/DeleteNodeInALinkedListTest.java
@@ -0,0 +1,19 @@
+package deletenode;
+
+import org.junit.jupiter.api.Test;
+
+class DeleteNodeInALinkedListTest {
+	DeleteNodeInALinkedList deleteNode = new DeleteNodeInALinkedList();
+	
+	@Test
+	void test_1() {
+	}
+	
+	
+	@Test
+	void test_2() {
+	}
+	
+	
+	
+}
