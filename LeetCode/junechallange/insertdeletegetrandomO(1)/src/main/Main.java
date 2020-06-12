181d7e3d327ba7782d3c754ae2cdac69e46232ca
Intercisa <sipiczkibarna@gmail.com>
2020-06-12 10:48:39 +0200

    insert delete getRandom O(1)

---
+++ LeetCode/junechallange/insertdeletegetrandomO(1)/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+/*
+ * Design a data structure that supports all following operations in average O(1) time.
+ * insert(val): Inserts an item val to the set if not already present.
+ * remove(val): Removes an item val from the set if present.
+ * getRandom: Returns a random element from current set of elements. 
+ * Each element must have the same probability of being returned.
+ * 
+ * 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ LeetCode/junechallange/insertdeletegetrandomO(1)/test/insertdeletegetrandom/RandomizedSetTest.java
@@ -0,0 +1,11 @@
+package insertdeletegetrandom;
+
+import org.junit.jupiter.api.Test;
+
+class RandomizedSetTest {
+
+	@Test
+	void test_1() {
+		
+	}
+}
