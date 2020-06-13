2b836c46e2f6d4f35d07d5eab72177521160b1fc
Intercisa <sipiczkibarna@gmail.com>
2020-06-13 19:16:45 +0200

    largest div subset

---
+++ LeetCode/junechallange/largestdivisiblesubset/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+/*
+ * 
+ * Given a set of distinct positive integers, 
+ * find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies:
+ * Si % Sj = 0 or Sj % Si = 0.
+ * If there are multiple solutions, return any subset is fine.
+ * 
+ * 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ LeetCode/junechallange/largestdivisiblesubset/test/largestdivisiblesubset/LargestDivisibleSubsetTest.java
@@ -0,0 +1,12 @@
+package largestdivisiblesubset;
+
+import org.junit.jupiter.api.Test;
+
+class LargestDivisibleSubsetTest {
+
+		
+	@Test
+	void test_1() {
+
+	}
+}
