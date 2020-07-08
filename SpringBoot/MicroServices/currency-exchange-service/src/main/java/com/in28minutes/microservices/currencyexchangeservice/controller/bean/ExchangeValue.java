95ccf5382217e244bbb3020fa5cd518393f0003c
Intercisa <sipiczkibarna@gmail.com>
2020-07-08 13:08:56 +0200

    exchange and conversion services

---
+++ SpringBoot/MicroServices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/bean/ExchangeValue.java
@@ -0,0 +1,48 @@
+package com.in28minutes.microservices.currencyexchangeservice.controller.bean;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeValue {
+	
+	@Id
+	private Long id;
+	
+	@Column(name = "currency_from")
+	private String from;
+	
+	@Column(name = "currency_to")
+	private String to;
+	
+	private BigDecimal conversionMultiple;
+	private int port;
+	
+	protected ExchangeValue() {};
+	
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+	public Long getId() {return id;}
+	public void setId(Long id) {this.id = id;}
+	public String getFrom() {return from;}
+	public void setFrom(String from) {this.from = from;}
+	public String getTo() {return to;}
+	public void setTo(String to) {this.to = to;}
+	public BigDecimal getConversionMultiple() {return conversionMultiple;}
+	public void setConversionMultiple(BigDecimal conversionMultiple) {this.conversionMultiple = conversionMultiple;}
+	public int getPort() {return port;}
+	public void setPort(int port) {this.port = port;}
+
+	@Override
+	public String toString() {
+		return String.format("ExchangeValue [id=%s, from=%s, to=%s, conversionMultiple=%s]", id, from, to,
+				conversionMultiple);
+	}
+}
---
+++ SpringBoot/MicroServices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/repo/ExchangeValueRepository.java
@@ -0,0 +1,11 @@
+package com.in28minutes.microservices.currencyexchangeservice.repo;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.in28minutes.microservices.currencyexchangeservice.controller.bean.ExchangeValue;
+
+public interface ExchangeValueRepository extends JpaRepositoryImplementation<ExchangeValue, Long>{
+
+	ExchangeValue findByFromAndTo(String from, String to);
+	
+}
