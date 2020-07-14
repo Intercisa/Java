32fae6ea18e30124944426cf6af60aa4448de8df
Intercisa <sipiczkibarna@gmail.com>
2020-07-14 13:15:02 +0200

    finnishing up

--- SpringBoot/MicroServices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/configcontroller/LimitsConfigurationController.java
+++ SpringBoot/MicroServices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/configcontroller/LimitsConfigurationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.configcontroller.bean.LimitConfiguration;
 import com.in28minutes.microservices.limitsservice.configproperties.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -17,5 +18,16 @@ public class LimitsConfigurationController {
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration") //if one of your services are going down your full service wont go with it -> with hystrix you can configure a default behavior  
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not Available");
+	} 
+	
+	//if one of your services are going down your full service wont go with it -> with hystrix you can configure a default behavior  
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
+	} 
 	
 }
