0d652ae1576c144b0e4aac5fa2f322c7dbb4a2d4
Intercisa <sipiczkibarna@gmail.com>
2020-08-24 17:36:28 +0200

    delete

--- temp/PaymentTypeRepository.java
+++
@@ -1,6 +0,0 @@
-package hu.ash.contractregistry.dal;
-
-import hu.ash.contractregistry.entity.task.PaymentTypeEntity;
-
-public interface PaymentTypeRepository extends BaseRepository<PaymentTypeEntity> {
-}
